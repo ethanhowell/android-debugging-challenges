e467c8981fee096045863d9299902eab6234b489
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-06-25 11:02:05 -0500

    fixed milestone 2

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -3,21 +3,28 @@ package com.codepath.debuggingchallenges.activities;
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.codepath.debuggingchallenges.R;
 
 public class ChangeBackgroundActivity extends AppCompatActivity {
+    private static String TAG = ChangeBackgroundActivity.class.getCanonicalName();
 
     private int oldColor = Color.BLUE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: created change background");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_background);
+        Button goButton = findViewById(R.id.btnGo);
     }
 
     public void onGoClick(View view) {
+        Log.i(TAG, "onGoClick: button clicked");
         View mainView = findViewById(android.R.id.content);
         mainView.setBackgroundColor(getNextColor());
     }
